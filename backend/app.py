4a82d64c5adb9104127c86afb3fa2771d5a89fe9
ALEX <asw236@cornell.edu>
2023-04-26 15:59:54 -0400

    deleted DB temp

--- backend/app.py
+++ backend/app.py
@@ -13,7 +13,7 @@ os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..",os.curdir))
 # Don't worry about the deployment credentials, those are fixed
 # You can use a different DB name if you want to
 MYSQL_USER = "root"
-MYSQL_USER_PASSWORD = ""
+MYSQL_USER_PASSWORD = "21Alshow!"
 MYSQL_PORT = 3306
 MYSQL_DATABASE = "boardgamesdb"
 
@@ -40,4 +40,4 @@ def home():
 
 
 
-# app.run(debug=True)
+#app.run(debug=True)
