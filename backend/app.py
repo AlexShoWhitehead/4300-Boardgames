cf90f2ed85e153638192f005d25f5a8cbb19cde2
ALEX <asw236@cornell.edu>
2023-05-01 11:32:27 -0400

    pushed sql

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask, render_template, request
 from flask_cors import CORS
 from helpers.MySQLDatabaseHandler import MySQLDatabaseHandler
-# from cosine import output, filter
+from cosine import output
 
 # ROOT_PATH for linking with all your files. 
 # Feel free to use a config.py or settings.py with a global export variable
@@ -28,6 +28,39 @@ CORS(app)
 # Sample search, the LIKE operator in this case is hard-coded, 
 # but if you decide to use SQLAlchemy ORM framework, 
 # there's a much better and cleaner way to do this
+def sql_search(age, length, players):
+    query_sql = f"""SELECT * FROM boardgames"""
+    if (age != '' and length == '' and players == ''):
+        query_sql = f"""SELECT * FROM boardgames WHERE (min_age <= {age})""" 
+    elif (age != '' and length != '' and players == ''):
+        query_sql = f"""SELECT * FROM boardgames WHERE (min_age <= {age}) 
+            AND (play_time <= {length})"""
+    elif (length != '' and age == '' and players == ''):
+        query_sql = f"""SELECT * FROM boardgames WHERE (play_time <= {length})"""
+    elif (length != '' and players != '' and age == ''):
+        query_sql = f"""SELECT * FROM boardgames WHERE )
+            (play_time <= {length})
+            AND (min_players <= {players})
+            AND (max_players >= {players})"""
+    elif (age != '' and length == '' and players != ''):
+        query_sql = f"""SELECT * FROM boardgames WHERE (min_age <= {age})
+            AND (min_players <= {players})
+            AND (max_players >= {players})"""
+    elif (length == '' and players != '' and age == ''):
+         query_sql = f"""SELECT * FROM boardgames WHERE (min_players <= {players})
+            AND (max_players >= {players})"""
+    elif (age != '' and length != '' and players != ''):
+        query_sql = f"""SELECT * FROM boardgames WHERE (min_age <= {age})
+            AND (play_time <= {length})
+            AND (min_players <= {players})
+            AND (max_players >= {players})"""
+    keys = ["id","name", "year_published", "min_players", "max_players", "play_time",
+            "min_age", "users_rated", "rating_average", "bgg_rank", "complexity_average",
+            "owned_users", "mechanics", "domains", "categories", "statistical_data",
+            "qualitative_data", "image_data", "users_commented", "comments"] 
+    data = mysql_engine.query_selector(query_sql)
+    return json.dumps([dict(zip(keys,i)) for i in data])
+
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
@@ -35,8 +68,8 @@ def home():
         query2 = request.form.get("ages")
         query3 = request.form.get("length")
         query4 = request.form.get("player_num")
-        return render_template('catalogue.html', tables=((query, query2, query3, query4)))
+        return render_template('catalogue.html', tables=(output(query, sql_search(age=query2, length=query3, players=query4))))
     return render_template('base.html', title="sample html")
 
 
-# app.run(debug=True)
+app.run(debug=True)
--- backend/cosine.py
+++ backend/cosine.py
@@ -1,29 +1,9 @@
 import pandas as pd
+import json
 import numpy as np
 import re
+import ast
 import csv
-
-
-def make_dataframe(csv_path, delimiter):
-    rows = []
-    with open(csv_path, newline='', encoding='UTF-8') as csvfile:
-        reader = csv.reader(csvfile, delimiter=delimiter, quotechar='"', quoting=csv.QUOTE_ALL)
-        next(reader, None)
-        for row in reader:
-            for col in range(len(row)):
-                row[col].replace("â€™", "'")
-            rows.append(row)
-        csvfile.close()
-
-    df = pd.DataFrame(rows)
-    df.columns = df.iloc[0]
-    df = df[1:]
-
-    return df
-
-description_data = pd.read_csv('datasets/Game_Data.csv')
-game_data =  make_dataframe('datasets/master_database.csv', ';')
-game_data = game_data.merge(description_data, on='name', how="right").drop("ID", axis='columns').dropna()
 
 def tokenize(text):
     """Returns a list of words that make up the text.
@@ -35,56 +15,8 @@ def tokenize(text):
     return re.findall(r"[a-zA-z]+", text.lower())
 # I want to make a dictionary containing every word in the descriptions and their counts
 
-def filter(age=0, length=0, players=2): 
-  """Filters out any games not fulfilling user needs
-  Returns: filtered game_data  
-  """
-  strAge = str(age)
-  strLength = str(length)
-  strPlayers = str(players)
-
-  ageFilter = game_data['min_age'] <= strAge
-  lengthFilter = game_data['play_time'] >= strLength
-  playersFilter = (game_data['min_players'] <= strPlayers)
-  players2Filter = (game_data['max_players'] >= strPlayers)
-
-  return game_data[ageFilter & lengthFilter & playersFilter & players2Filter]
-
 word_count = {}
-
-descriptions = game_data['description'].astype('string').to_numpy()
-comments = game_data['comments'].astype('string').to_numpy()
-names = game_data['name'].astype('string').to_numpy()
-
 token_list = []
-
-for description in descriptions:
-  tokens = tokenize(description)
-  for token in tokens:
-    token_list.append(token)
-
-for comment in comments:
-  tokens = tokenize(comment)
-  for token in tokens:
-    token_list.append(token)
-
-for word in token_list:
-  if word in word_count.keys():
-    word_count[word] += 1
-  else:
-    word_count.update({word: 1})
-
-# Now I want to find the good types, since this is just a proto type
-# I'm just going to say that words that occur more then 200 times are good
-# In the future we should look at word frequencies to better fine tune which
-# Words are considered good words
-# These parameters can obviously be changed later
-good_types = []
-for word in word_count.keys():
-  if word_count[word] >= 200:
-    good_types.append(word)
-
-good_types.sort()
 
 # Now I'll make a function that takes in text and turns it into a vector
 # representation of good types
@@ -96,15 +28,6 @@ def vectorize(text, good_types, tokenizer):
     if word in good_types:
       vector[good_types.index(word)] += 1
   return vector
-# From here we can make vector representations for all the descriptions
-description_vectors = []
-for description in descriptions:
-  description_vectors.append(vectorize(description, good_types, tokenize))
-
-# ^ this code can take a while to run
-
-# now that we have the descriton vectors, we will make a function that
-# calculates the cosine similarity between a query and description vector
 
 def cosine_sim(query, vector):
   query_norm = np.linalg.norm(query)
@@ -113,69 +36,66 @@ def cosine_sim(query, vector):
 
   return dot_product / (query_norm * vector_norm)
 
-# from here we will make a function that takes in a query and returns the top
-# 20 results
-
-images = game_data['image_data'].astype('string').to_numpy()
-average_ratings = game_data['rating_average'].astype('string').to_numpy()
-categories = game_data['rating_average'].astype('string').to_numpy()
-
-def make_matrix(query, a=1):
+def get_ranked_list(query, names, description_vectors, descriptions, comments, images, average_ratings, categories, num_of_results = 20):
   sim_scores = np.empty(len(names))
+
   for i in range(len(description_vectors)):
-    sim_scores[i] = a * cosine_sim(query, description_vectors[i])
-  return sim_scores
-
-def rocchio(initQuery, relevant, irrelevant, a=.3, b=.3, c=.8, clip = True):
-  
-  sim_scores = make_matrix(initQuery, a)
+    sim_scores[i] = cosine_sim(query, description_vectors[i])
   sorted_index = np.argsort(sim_scores)
-
-  if not len(relevant) == 0:
-    secondPre = b * (1 / len(relevant))
-    secondSum = sorted_index[relevant[0]]
-    for d in range(len(relevant) - 1):
-      secondSum = secondSum + sorted_index[relevant[d + 1]]
-      secondSum = secondPre * secondSum
-      initQuery = initQuery + secondSum
-    
-  if not len(irrelevant) == 0:
-    thirdPre = c * (1 / len(irrelevant))
-    thirdSum =sorted_index[irrelevant[0]]
-    for i in range(len(irrelevant) - 1):
-      thirdSum = thirdSum + sorted_index[irrelevant[i + 1]]
-      thirdSum = thirdPre * thirdSum
-      initQuery = initQuery - thirdSum
-    
-  if(clip):
-    for j in range(len(initQuery)):
-      if initQuery[j] < 0:
-        initQuery[j] = 0
-    
-  return initQuery
-
-def get_ranked_list(query, relevant_in, irrelevant_in, num_of_results = 20):
-  tempList = []
-
-  for i in range(len(relevant_in)):
-        movie = names[relevant_in[i]]
-        tempList.append=(rocchio(movie, relevant_in[i], irrelevant_in[i]))
 
   ranked_list = []
   for i in range(num_of_results):
-    index = tempList[len(tempList) - 1 - i]
-    return_vals = (names[index], descriptions[index], images[index], average_ratings[index], categories[index])
+    index = sorted_index[len(sorted_index) - 1 - i]
+    return_vals = (names[index], descriptions[index], comments[index], images[index], average_ratings[index], categories[index])
     ranked_list.append(return_vals)
 
   return ranked_list
 
 # finally we should be able to take in the query and return the most similar
 # games
-def output(q, ages, len, player):
+def output(query, database):
+  game_data = json.loads(database)
+  word_count = {}
+  descriptions = []
+  comments = []
+  names = []
+  images = []
+  avg_ratings = []
+  categories = []
 
-  filter(ages, len, player)
+  for game in range(len(game_data)): 
+    descriptions.append(str(game_data[game]['qualitative_data']))
+    comments.append(str(game_data[game]['comments']))
+    names.append(str(game_data[game]['name']))
+    images.append(str(game_data[game]['image_data']))
+    avg_ratings.append(str(game_data[game]['rating_average']))
+    categories.append(str(game_data[game]['categories']))
 
-  query = vectorize(q, good_types, tokenize)
-  ranked_list = get_ranked_list(query)
+  for description in descriptions:
+    tokens = tokenize(description)
+    for token in tokens:
+      token_list.append(token)
 
-  return ranked_list
+  for comment in comments:
+    tokens = tokenize(comment)
+    for token in tokens:
+      token_list.append(token)
+
+  for word in token_list:
+    if word in word_count.keys():
+      word_count[word] += 1
+    else:
+      word_count.update({word: 1})
+
+  good_types = []
+  for word in word_count.keys():
+    if word_count[word] >= 0:
+      good_types.append(word)
+  good_types.sort()
+  query = vectorize(query, good_types, tokenize)
+
+  description_vectors = []
+  for description in descriptions:
+    description_vectors.append(vectorize(description, good_types, tokenize))
+
+  return get_ranked_list(query, names, description_vectors, descriptions, comments, images, avg_ratings, categories, num_of_results = 3)
