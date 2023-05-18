76413aa16b1aef07a4dc8ac1ae76df6d7b5bd151
ALEX <asw236@cornell.edu>
2023-05-18 18:05:30 -0400

    reverted docker files

--- backend/helpers/MySQLDatabaseHandler.py
+++ backend/helpers/MySQLDatabaseHandler.py
@@ -2,21 +2,23 @@ import os
 import sqlalchemy as db
 
 class MySQLDatabaseHandler(object):
-    
-    IS_DOCKER = True if 'DB_NAME' in os.environ else False
 
     def __init__(self,MYSQL_USER,MYSQL_USER_PASSWORD,MYSQL_PORT,MYSQL_DATABASE,MYSQL_HOST = "localhost"):
-        
-        self.MYSQL_HOST = os.environ['DB_NAME'] if MySQLDatabaseHandler.IS_DOCKER else MYSQL_HOST
-        self.MYSQL_USER = "admin" if MySQLDatabaseHandler.IS_DOCKER else MYSQL_USER
-        self.MYSQL_USER_PASSWORD = "admin" if MySQLDatabaseHandler.IS_DOCKER else MYSQL_USER_PASSWORD
-        self.MYSQL_PORT = 3306 if MySQLDatabaseHandler.IS_DOCKER else MYSQL_PORT
-        self.MYSQL_DATABASE = "4300_DATABASE" if MySQLDatabaseHandler.IS_DOCKER else MYSQL_DATABASE
+        self.IS_DOCKER = True if 'DB_NAME' in os.environ else False
+        self.MYSQL_HOST = os.environ['DB_NAME'] if self.IS_DOCKER else MYSQL_HOST
+        self.MYSQL_USER = "admin" if self.IS_DOCKER else MYSQL_USER
+        self.MYSQL_USER_PASSWORD = "admin" if self.IS_DOCKER else MYSQL_USER_PASSWORD
+        self.MYSQL_PORT = 3306 if self.IS_DOCKER else MYSQL_PORT
+        self.MYSQL_DATABASE = MYSQL_DATABASE
         self.engine = self.validate_connection()
 
     def validate_connection(self):
-        print(f"mysql+pymysql://{self.MYSQL_USER}:{self.MYSQL_USER_PASSWORD}@{self.MYSQL_HOST}:{self.MYSQL_PORT}/{self.MYSQL_DATABASE}")
-        return db.create_engine(f"mysql+pymysql://{self.MYSQL_USER}:{self.MYSQL_USER_PASSWORD}@{self.MYSQL_HOST}:{self.MYSQL_PORT}/{self.MYSQL_DATABASE}")
+
+        engine = db.create_engine(f"mysql+pymysql://{self.MYSQL_USER}:{self.MYSQL_USER_PASSWORD}@{self.MYSQL_HOST}:{self.MYSQL_PORT}")
+        conn = engine.connect()
+        conn.execute(f"CREATE DATABASE IF NOT EXISTS {self.MYSQL_DATABASE}")
+        conn.execute(f"USE {self.MYSQL_DATABASE}")
+        return engine
 
     def lease_connection(self):
         return self.engine.connect()
@@ -29,14 +31,13 @@ class MySQLDatabaseHandler(object):
         else:
             conn.execute(query)
         
-
     def query_selector(self,query):
         conn = self.lease_connection()
         data = conn.execute(query)
         return data
 
     def load_file_into_db(self,file_path  = None):
-        if MySQLDatabaseHandler.IS_DOCKER:
+        if self.IS_DOCKER:
             return
         if file_path is None:
             file_path = os.path.join(os.environ['ROOT_PATH'],'init.sql')
